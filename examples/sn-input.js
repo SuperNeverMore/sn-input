a3859f7d27cf0ca0fadd64b19a25ba1f4eaeb5c5
caoqianjie <caoqianjie008@163.com>
2017-11-03 16:18:48 +0800

    input init

---
+++ examples/sn-input.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import Demo from './sn-input.vue';
+
+new Vue({ // eslint-disable-line
+  el: '#app',
+  render: h => h(Demo)
+});
---
+++ examples/sn-input.vue
@@ -0,0 +1,13 @@
+<template>
+  <Demo></Demo>
+</template>
+
+<script>
+  import Demo from '../src/sn-input/sn-input';
+  export default {
+    name: 'app',
+    components: {
+      Demo
+    }
+  };
+</script>
